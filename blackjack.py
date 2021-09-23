0c2517778a390a694ef7c38184f1f8231f7c9e00
Fabio <fabiopires1011@gmail.com>
2021-09-23 22:49:03 +0100

    Player turn#

--- blackjack.py
+++ blackjack.py
@@ -1,6 +1,7 @@
 from card import Card
 from player import Player
 from deck import Deck
+import os
 
 
 class BlackJack:
@@ -11,6 +12,7 @@ class BlackJack:
 
     def play(self):
         self.start_game()
+        self.player_turn()
 
     def start_game(self):
         self.deck.fill_deck()
@@ -26,6 +28,31 @@ class BlackJack:
         Card.print_downed_card()
         self.dealer.calculate_dealer_score_first_round()
         print(f"Dealer's Score: {self.dealer.count}")
+
+    def player_turn(self):
+        draw = True
+        while draw and self.player.count < 21:
+            print("Hit (H) or Stand (S)")
+            response = input()
+            if response == 'S':
+                draw = False
+            else:
+                BlackJack.clear_screen()
+                self.player.hit_me(self.deck)
+                self.player.print_player_cards()
+                print(f'Player score: {self.player.count}')
+                if self.player.count == 21:
+                    print('BLACKJACK!')
+                elif self.player.count > 21:
+                    print('BUSTED!')
+                self.dealer.print_first_card()
+                Card.print_downed_card()
+                self.dealer.calculate_dealer_score_first_round()
+                print(f"Dealer's Score: {self.dealer.count}")
+
+    @staticmethod
+    def clear_screen():
+        os.system('cls' if os.name == 'nt' else 'clear')
 
 
 blackjack = BlackJack()
