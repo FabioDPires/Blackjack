0fa8a0dbc00cf4654e8506ebefab6fd8afdfd213
Fabio <fabiopires1011@gmail.com>
2021-09-23 20:22:19 +0100

    Show card method

--- Card.py
+++ Card.py
@@ -4,10 +4,22 @@ class Card:
         self.value = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'][value - 1]
         self.suit = '♥♦♣♠'[suit]
 
-    def getPrice(self):
+    # Note: in Python is a good practise that functions' names should be all lowercase. So if a function's name has
+    # more than one word they should be separated by a _
+    def get_price(self):
 
         if self.cost >= 10:
             return 10;
         elif self.cost == 1:
             return 11
         return self.cost
+
+    def draw_card(self):
+        print('┌───────┐')
+        print(f'| {self.value:<2}    |')
+        print('|       |')
+        print(f'|   {self.suit}   |')
+        print('|       |')
+        print(f'|    {self.value:>2} |')
+        print('└───────┘') 
+
